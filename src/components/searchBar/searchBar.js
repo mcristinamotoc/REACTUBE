63d01d2db75c7b9c2cb9268fa832da935b51860f
Cristina M <mcristinamotoc@gmail.com>
2021-02-09 13:25:07 +0100

    searchBarComponent

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
 import { TextField } from "@material-ui/core";
+import useStyles from "./searchBar_Style";
 
 const SearchBar = ({ onFormSubmit }) => {
+  const [termFromSearchBar, setSearchTerm] = useState("");
+
+  const classes = useStyles();
+
+  const handleChange = (event) => setSearchTerm(event.target.value);
+
+  const handleSubmit = (event) => {
+    if (event.key === "Enter") {
+      onFormSubmit(termFromSearchBar);
+    }
+  };
+
   return (
-    <div>
-      <TextField label="Search..." />
+    <div className={classes.searchBar}>
+      <form onSubmit={handleSubmit} 
+      className={classes.searchForm}>
+        <TextField
+          className={classes.searchTextField}
+          label="Search"
+          value={termFromSearchBar}
+          onChange={handleChange}
+          onKeyPress={handleSubmit}
+        />
+        <IconButton 
+        type="submit" 
+        onSubmit={handleChange} 
+        aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </form>
     </div>
   );
 };
 
 export default SearchBar;
-
-
-
---
+++ src/components/searchBar/searchBar_Style.js
@@ -0,0 +1,29 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((style) => ({
+  searchBar: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#ffe680",
+    padding: "1rem 1rem",
+  },
+  searchForm:{
+    
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    width:'30%',
+    border: '1px solid',
+    margin:'auto',
+    padding:'0.5rem',
+  },
+  searchTextField:{
+      width:'100%',
+      marginLeft:'1rem',
+  }
+}))
+
+
+
+
+
